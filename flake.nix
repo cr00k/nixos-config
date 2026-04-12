d9d476b8aecd9982b58bf3391fd075ca6cb342ed
cr00k <rok@rkokalj.si>
2026-04-13 00:06:55 +0200

    hm-backup

--- flake.nix
+++ flake.nix
@@ -34,6 +34,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "hm-backup";
               home-manager.users.rok = import homePath;
             }
           ];
